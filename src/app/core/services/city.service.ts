40d794fc2b36ebd7db2b9af8c178b6fd6411fe20
bratosinm <72658836+bratosinm@users.noreply.github.com>
2022-12-08 22:52:05 +0200

    2 files modified

--- src/app/core/services/city.service.ts
+++ src/app/core/services/city.service.ts
@@ -2,13 +2,14 @@ import { Injectable } from '@angular/core';
 import { CITIES } from '../../mocks/cities-mock';
 import { City } from '../interfaces/city';
 
-@Injectable()
+@Injectable({
+  providedIn:'root'
+})
 export class CityService {
   constructor() {}
 
   getCities(): Array<City> {
     return CITIES;
   }
-  
-}
 
+}
